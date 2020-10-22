a44dd0bbffccb64d421091800b18d6fc0b6a53f0
Zedd1558 <zahidulislam@iut-dhaka.edu>
2020-10-22 18:43:19 +0600

    added train_classifier.py

---
+++ train_classifier.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+
+def train(opt):
+
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    data_dir = '/content/classifier_data'
+    image_data = datasets.ImageFolder(data_dir + '/train/')
+    
+    num_data = len(image_data)
+    indices = list(range(image_train))
+    np.random.seed(42)   # shuffle using seed, as we want same val data every time 
+    np.random.shuffle(indices)
+    split = int(np.floor(valid_size * num_train))
+    train_idx, valid_idx = indices[split:], indices[:split]
+    
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                shuffle=True, num_workers=4)
+                for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+    # load model
+
+    # train model
+
+    # save 
+
+
+    #         # Update best fitness
+
+
+    #         # Save model
+    #         save = (not opt.nosave) or (final_epoch and not opt.evolve)
+    #         if save:
+    #             with open(results_file, 'r') as f:  # create checkpoint
+    #                 ckpt = {'epoch': epoch,
+    #                         'best_fitness': best_fitness,
+    #                         'training_results': f.read(),
+    #                         'model': ema.ema,
+    #                         'optimizer': None if final_epoch else optimizer.state_dict()}
+
+    #             # Save last, best and delete
+    #             torch.save(ckpt, last)
+    #             if best_fitness == fi:
+    #                 torch.save(ckpt, best)
+    #                 try:
+    #                     if opt.SaveBestInDrive != 'NOT_SET':
+    #                         torch.save(ckpt, opt.SaveBestInDrive)
+    #                 except:
+    #                     print("!could not save best model to",opt.SaveBestInDrive)
+    #             del ckpt
+    #     # end epoch ----------------------------------------------------------------------------------------------------
+    # # end training
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
+    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
+    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
+    parser.add_argument('--SaveBestInDrive', type=str, default='NOT_SET', help='saves the best model in given google drive path')
+    parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
+    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
+    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
+    parser.add_argument('--rect', action='store_true', help='rectangular training')
+    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
+    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
+    parser.add_argument('--notest', action='store_true', help='only test final epoch')
+    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
+    parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
+    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
+    parser.add_argument('--cache-images', action='store_true', help='cache images for faster training')
+    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
+    parser.add_argument('--name', default='', help='renames experiment folder exp{N} to exp{N}_{name} if supplied')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
+    parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
+    parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
+    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
+    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
+    parser.add_argument('--logdir', type=str, default='runs/', help='logging directory')
+    parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
+    opt = parser.parse_args()
+
+    # Resume
+    if opt.resume: 
+        opt.model
+    else:
+        opt.cnn_type 
+        opt.weight 
+    
+    # Train
+    train(opt)
+

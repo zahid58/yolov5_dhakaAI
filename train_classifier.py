867c36323e2b78d200749c3ec5c0eb4a79d97186
Md. Zahidul Islam <zahidulislam@iut-dhaka.edu>
2020-10-23 20:26:34 +0600

    helpful print functions

--- train_classifier.py
+++ train_classifier.py
@@ -15,18 +15,20 @@ import albumentations as A
 from efficientnet_pytorch import EfficientNet
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs,dataloaders,device,dataset_sizes):
+
     since = time.time()
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
     start_epoch = 0
     
     if opt.resume != 'False':
+        print('> loading resumed weights from',opt.resume)
         checkpoint = torch.load(opt.resume)
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         start_epoch = checkpoint['epoch']
         model.load_state_dict(checkpoint['model_state_dict'])
         
-
+    print('> starting training...')
     for epoch in range(start_epoch,num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
@@ -78,15 +80,19 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs,dataloaders,d
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': loss,
             }
+
             if phase=='train':
                 torch.save(checkpoint,os.path.join(checkpoint_save_dir,'last.pt'))
+
             # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
+                torch.save(checkpoint, os.path.join(checkpoint_save_dir,'best.pt'))
+
                 if opt.SaveBestInDrive !='NOT_SET':
                    torch.save(best_model_wts, opt.SaveBestInDrive)
-                   torch.save(checkpoint, os.path.join(checkpoint_save_dir,'best.pt'))
+                   
         print()
 
     time_elapsed = time.time() - since
@@ -114,7 +120,7 @@ def albumen_augmentations():
 
 
 def load_model(type='efficientnet', num_classes = 21):
-    
+ 
     if type=='resnet34':
         model = models.resnet34(pretraind = True)
     elif type=='resnet18':
@@ -126,7 +132,7 @@ def load_model(type='efficientnet', num_classes = 21):
     elif type=='efficientnet':
         model = EfficientNet.from_pretrained('efficientnet-b3')
     else:
-        raise Exception("Sorry, CNN_TYPE not recognized!")
+        raise Exception("! Sorry, CNN_TYPE not recognized !")
         
     # change classifier
     if type == 'efficientnet':
@@ -160,24 +166,30 @@ def train(opt):
     }
     
     data_dir = opt.data_dir
+    print('> loading data from',data_dir)
     image_datasets = {x: ImageFolder(os.path.join(data_dir, x),data_transforms[x]) for x in ['train','val']}
     traffics = {'truck':0, 'pickup':1, 'car':2, 'suv':3, 'three wheelers (CNG)':4, 'bus':5, 'van':6, 'ambulance':7, 'rickshaw':8, 'minivan':9, 'motorbike':10, 'bicycle':11, 'army vehicle':12, 'human hauler':13, 'taxi':14, 'wheelbarrow':15, 'auto rickshaw':16, 'minibus':17, 'scooter':18, 'policecar':19, 'garbagevan':20}
     image_datasets['train'].class_to_idx = traffics
     image_datasets['val'].class_to_idx = traffics
-    
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batch_size,shuffle=True, num_workers=4) for x in ['train', 'val']}    
+
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    print('> num images in train and val folder:',dataset_sizes)
     class_names = image_datasets['train'].classes
+    print('> classes :', class_names)
+
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batch_size,shuffle=True, num_workers=4) for x in ['train', 'val']}    
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print('> training on device:',device)
 
     # load model
-
     model_ft = None
+    print('> loading {} model, number of classes is {}....'.format(opt.CNN_type,len(class_names)))
     model_ft = load_model(type = opt.CNN_type, num_classes = len(class_names))
 
     # compile model
-    
-    for param in model.parameters():
+    print('> compiling...')
+    for param in model_ft.parameters():
         param.requires_grad = True
     
     model_ft = model_ft.to(device)
@@ -203,54 +215,14 @@ if __name__ == '__main__':
     parser.add_argument('--CNN_type', type=str, help='cnn type to be trained')
     opt = parser.parse_args()
     
+    print('> opt:', opt)
+    
     checkpoint_save_dir = 'classifier_checkpoints'
     if not os.path.exists(checkpoint_save_dir):
         os.mkdir(checkpoint_save_dir)
+    print('> last and best model will be saved inside',checkpoint_save_dir)
 
     # Train
     train(opt)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
-    parser.add_argument('--SaveBestInDrive', type=str, default='NOT_SET', help='saves the best model in given google drive path')
-    parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
-    parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
-    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
-    parser.add_argument('--notest', action='store_true', help='only test final epoch')
-    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
-    parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
-    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
-    parser.add_argument('--cache-images', action='store_true', help='cache images for faster training')
-    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
-    parser.add_argument('--name', default='', help='renames experiment folder exp{N} to exp{N}_{name} if supplied')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
-    parser.add_argument('--single-cls', action='store_true', help='train as single-class dataset')
-    parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
-    parser.add_argument('--logdir', type=str, default='runs/', help='logging directory')
-    parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')"""

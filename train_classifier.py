71c01aef35d1678b01d04fa135300840230af127
Md. Zahidul Islam <zahidulislam@iut-dhaka.edu>
2020-10-24 18:20:53 +0600

    Update train_classifier.py

--- train_classifier.py
+++ train_classifier.py
@@ -14,6 +14,7 @@ import argparse
 import albumentations as A
 from efficientnet_pytorch import EfficientNet
 from tqdm import tqdm
+from PIL import Image
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs,dataloaders,device,dataset_sizes):
 
